3696e1c243d413bff4fc0b007505bacc72451928
Shannon Lake <shannonsivertsen@gmail.com>
2022-12-08 21:00:16 -0500

    v10

--- realestate.js
+++ realestate.js
@@ -2,8 +2,9 @@ let ans = 0;
 fetch("WEEK6_JSON.json")
     .then(response => response.json())
     .then(estRec => {
-        let li = `<tr><th>Parcel</th>
-                      <th>Hello Date</th>
+        let li = `<tr><th>Image</th>
+                      <th>Parcel</th>
+                      <th>Last Sell Date</th>
                       <th>Owner Name</th>
                       <th>Address</th>
                       <th>Qtly Pay Avg</th>
@@ -18,6 +19,7 @@ fetch("WEEK6_JSON.json")
             address = house.owner_address.street + "<br>" + house.owner_address.city + "<br>" + house.owner_address.state + ", " + house.owner_address.zip;
             avg = (house.quarter_payments[0] + house.quarter_payments[1] + house.quarter_payments[2] + house.quarter_payments[3])/4;
             li += `<tr>
+                <td>${house.image} </td>
                 <td>${house.parcel} </td>
                 <td>${date}</td>
                 <td>${name}</td>
@@ -30,27 +32,3 @@ fetch("WEEK6_JSON.json")
         });
     document.getElementById("realest").innerHTML = li;
 });
-
-
-//                   {
-//                    "parcel" : 123566,
-//                    "deed_date" : "1999-07-08T09:02:23.899Z",
-//                    "owner_name" : { "first" : "Mary",
-//                                     "last" : "Lamb"
-//                                   },
-//                    "owner_address" :  {   "street": "11 West Lane",
-//                                           "city": "Acushnet",
-//                                           "state": "MA",
-//                                           "zip": 34556
-//                                       },
-//                    "assessments" :
-//                                   {   "land_value": 990000,
-//                                       "building_value": 1000456
-//                                   },
-//                   "quarter_payments": [1000, 9999, 8888, 9998],
-//                   "paid_YTD": 120000,
-//                   "tax_rate_perK": 98
-//
-//                 }
-//
-//           ]
