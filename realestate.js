a760500b0729bb7b142c696d40f675d39a4efdfa
Shannon Lake <shannonsivertsen@gmail.com>
2022-12-10 08:56:05 -0500

    V16

--- realestate.js
+++ realestate.js
@@ -13,13 +13,14 @@ fetch("WEEK6_JSON.json")
                       <th>Total Values</th></tr>`;
 
        estRec.forEach(house => {
+            image = house.image;
             ans = house.assessments.land_value + house.assessments.building_value;
             date = house.deed_date.substr(0,10);
             name = house.owner_name.first + " " + house.owner_name.last;
             address = house.owner_address.street + "<br>" + house.owner_address.city + "<br>" + house.owner_address.state + ", " + house.owner_address.zip;
             avg = (house.quarter_payments[0] + house.quarter_payments[1] + house.quarter_payments[2] + house.quarter_payments[3])/4;
             li += `<tr>
-                <td>${house.image} </td>
+                <td>${image} </td>
                 <td>${house.parcel} </td>
                 <td>${date}</td>
                 <td>${name}</td>
