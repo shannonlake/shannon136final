be2d199717afd873a2be412998b2142c94d2bf15
Shannon Lake <shannonsivertsen@gmail.com>
2022-12-10 09:54:25 -0500

    V21

--- realestate.js
+++ realestate.js
@@ -11,16 +11,16 @@ fetch("WEEK6_JSON.json")
             name = house.owner_name.first + " " + house.owner_name.last;
             address = house.owner_address.street + "<br>" + house.owner_address.city + "<br>" + house.owner_address.state + ", " + house.owner_address.zip;
             avg = (house.quarter_payments[0] + house.quarter_payments[1] + house.quarter_payments[2] + house.quarter_payments[3])/4;
-            div += `<div>
+            div += `<div class="houseblock">
                 <div><img src="${image}" height="300" width="300"></div>
-                <h3>Parcel ID</h3><p>${house.parcel}</p>
-                <h3>Last Sell Date ID</h3><p>${date}</p>
-                <h3>Owner Name</h3><p>${name}</p>
-                <h3>Address</h3><p>${address}</p>
-                <h3>Mort. Est.</h3><p>${avg}</p>
-                <h3>Beds</h3><p>${house.beds}</p>
-                <h3>Baths</h3><p>${house.baths}</p>
-                <h3>Est. Value</h3><p>${ans}</p>
+                <p><strong>Parcel ID:</strong> ${house.parcel}</p>
+                <p><strong>Last Sell Date ID:</strong> ${date}</p>
+                <p><strong>Owner Name:</strong> ${name}</p>
+                <p><strong>Address:</strong> ${address}</p>
+                <p><strong>Mort. Est:</strong> ${avg}</p>
+                <p><strong>Beds:</strong> ${house.beds}</p>
+                <p><strong>Baths:</strong> ${house.baths}</p>
+                <p><strong>Est. Value:</strong> ${ans}</p>
             </div>`;
         });
     document.getElementById("realest").innerHTML = div;
