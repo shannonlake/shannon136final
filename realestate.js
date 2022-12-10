f30568d1c1831e6501f599c1a3b7faed543fdbef
Shannon Lake <shannonsivertsen@gmail.com>
2022-12-10 09:23:06 -0500

    V19

--- realestate.js
+++ realestate.js
@@ -2,15 +2,6 @@ let ans = 0;
 fetch("WEEK6_JSON.json")
     .then(response => response.json())
     .then(estRec => {
-        let div = `<tr><th>Image</th>
-                      <th>Parcel</th>
-                      <th>Last Sell Date</th>
-                      <th>Owner Name</th>
-                      <th>Address</th>
-                      <th>Qtly Pay Avg</th>
-                      <th>Paid YTD</th>
-                      <th>Tax Rate/1000</th>
-                      <th>Total Values</th></tr>`;
 
        estRec.forEach(house => {
             image = house.image;
@@ -19,17 +10,17 @@ fetch("WEEK6_JSON.json")
             name = house.owner_name.first + " " + house.owner_name.last;
             address = house.owner_address.street + "<br>" + house.owner_address.city + "<br>" + house.owner_address.state + ", " + house.owner_address.zip;
             avg = (house.quarter_payments[0] + house.quarter_payments[1] + house.quarter_payments[2] + house.quarter_payments[3])/4;
-            li += `<tr>
-                <td><img src="${image}" height="100" width="100"></td>
-                <td>${house.parcel} </td>
-                <td>${date}</td>
-                <td>${name}</td>
-                <td>${address}</td>
-                <td>${avg}</td>
-                <td>${house.paid_YTD}</td>
-                <td>${house.tax_rate_perK}</td>
-                <td>${ans}</td>
-            </tr>`;
+            div = `<div>
+                <div><img src="${image}" height="300" width="300"></div>
+                <h3>Parcel ID</h3><p>${house.parcel}</p>
+                <h3>Last Sell Date ID</h3><p>${date}</p>
+                <h3>Owner Name</h3><p>${name}</p>
+                <h3>Address</h3><p>${address}</p>
+                <h3>Mort. Est.</h3><p>${avg}</p>
+                <h3>Beds</h3><p>${house.beds}</p>
+                <h3>Baths</h3><p>${house.baths}</p>
+                <h3>Est. Value</h3><p>${ans}</p>
+            </div>`;
         });
-    document.getElementById("realest").innerHTML = li;
+    document.getElementById("realest").innerHTML = div;
 });
