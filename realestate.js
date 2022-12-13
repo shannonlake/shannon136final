9c9ad0c479433927aaccb6f36836c35b836ec000
Shannon Lake <shannonsivertsen@gmail.com>
2022-12-13 18:58:47 -0500

    v87

--- realestate.js
+++ realestate.js
@@ -31,13 +31,13 @@ fetch("houses.json")
             <div id="${house.parcel}" class="overlay" onclick="this.style.display='none'">
             <div id="listing-info">
             <div id="overlayimg"><img src="${image}"></div>
-            <p><span class="overlayprice">$</strong>${ans}<span></br>
+            <p><span class="overlayprice">$</strong>${ans}</span></br>
             <strong>Parcel ID:</strong> ${house.parcel}</br>
             <strong>Mort. Est:</strong> ${avg}</br>
             <strong>Owner Name:</strong> ${name}</br>
             <strong>Last Sold:</strong> ${date}</br>
             </p>
-            </div>
+
 
 
             <div id="rightside">
@@ -50,6 +50,7 @@ fetch("houses.json")
                 <input class="inputform" type="text" id="email" name="email" placeholder="Email"><br><br>
                 <input class="btn btn-primary listingbtn" type="submit" value="Submit">
               </form>
+              </div>
               </div>
               </div>
               </div>`;
